8c25b4cce5b94038a5dbd6e899d2e6b1fe7d4a61
Mike Cao <mc2522@rit.edu>
2020-05-30 20:36:28 -0400

    finished setup

---
+++ src/Note.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Note extends Component {
+    constructor() {
+        super()
+        this.state = {
+            sample: 0
+        }
+    }
+}
+
+export default Note
