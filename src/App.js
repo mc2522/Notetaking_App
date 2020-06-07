cecd08bdbcd44092f63d342b2a2e878cfaf3d600
Mike Cao <mc2522@rit.edu>
2020-06-06 21:49:19 -0400

    added state to App that keeps track of notes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,40 @@ import Note from './Note'
 import CreateNote from './CreateNote'
 import './App.css'
 
-function App() {
-  	return (
-    	<div className="App">
-			<Note title="Sample" text="sample" />
-			<Note title="Sample2" text="sample2" />
-			<CreateNote />
-    	</div>
-  	)
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			// array of objects with properties of title and text that represents notes
+			notes: []
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+
+	// this method handles submits from the createNote form
+	handleSubmit() {
+		this.setState( (prevState) => {
+			return (
+				prevState.notes.append()
+			)
+		})
+	}
+
+	render() {
+		// maps the notes in state to another array with every element as a component
+		const noteComponents = this.state.notes.map((note) => {
+			return <Note title={note.title} text={note.text} />
+		})
+		noteComponents.push(<Note title="example note" text="sample text" />)
+
+		return (
+			<div className="App">
+				{/* array of note components */}
+				{noteComponents}
+				<CreateNote onChange={this.handleSubmit} />
+			</div>
+		)
+	}
 }
 
 export default App
